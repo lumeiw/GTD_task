fd4a917bb98d2656a071b616eeb448c4a4b51490
Deniska <weqweewee@mail.ru>
2025-03-02 19:55:59 +0500

    Добавил регистрацию кубита + чутка исправил

--- lib/features/task/presentation/cubits/details/details_task_state.dart
+++ lib/features/task/presentation/cubits/details/details_task_state.dart
@@ -1,10 +1,15 @@
+import 'package:gtd_task/features/task/domain/entities/i_task_entity.dart';
+
 sealed class TaskDetailsState {}
 
 class TaskDetailsInitial extends TaskDetailsState {}
 
 class TaskDetailsLoading extends TaskDetailsState {}
 
-class TaskDetailsSuccess extends TaskDetailsState {}
+class TaskDetailsLoaded extends TaskDetailsState {
+  final ITaskEntity task;
+  TaskDetailsLoaded(this.task);
+}
 
 class TaskDetailsError extends TaskDetailsState {
   final String message;
